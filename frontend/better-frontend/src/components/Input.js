1f764f40d49eeff5f3bcb5c28dad102dbed7eae6
farid <farid@haiderzada.de>
2021-05-18 10:57:51 +0200

    Designed all Buttons

--- frontend/better-frontend/src/components/Input.js
+++ frontend/better-frontend/src/components/Input.js
@@ -1,10 +1,36 @@
+import styled from "styled-components/macro";
+
 function Input({addToDo}){
     let inputText = ""
     return(
-    <div>
-        <input onChange={(event) => inputText = event.target.value}/>
-        <button onClick ={() => addToDo( inputText )}>Add</button>
-    </div>
+    <Footer>
+        <InputField onChange={(event) => inputText = event.target.value}/>
+        <AddButton onClick ={() => addToDo( inputText )}>Add</AddButton>
+    </Footer>
     )
 }
 export default Input
+
+const Footer = styled.text`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+`
+
+const AddButton = styled.button`
+  width: 100px;
+  height: 48px;
+  border-radius: 12px;
+  background-color: #f1316b;
+  color: black;
+  margin-top: 45px;
+`
+const InputField = styled.input`
+  height: 40px;
+  border-radius: 12px;
+  background-color: lightseagreen;
+  color: black;
+  
+`
--- frontend/better-frontend/src/components/ToDoCard.js
+++ frontend/better-frontend/src/components/ToDoCard.js
@@ -1,16 +1,41 @@
+import styled from "styled-components/macro";
 
 function ToDoCard ({toDo, deleteTodo, updateTodo}) {
     return(
+
         <div>
             <p>{toDo.description}</p>
             <p>{toDo.id}</p>
             <p>{toDo.status}</p>
-            <button onClick={() => deleteTodo(toDo.id)}>Delete</button>
-            <button>Details</button>
-            <button onClick={() => updateTodo(toDo)} disabled={toDo.status === "DONE"}>Advance</button>
+            <RedButton onClick={() => deleteTodo(toDo.id)}>Delete</RedButton>
+            <YellowButton>Details</YellowButton>
+            <GreenButton onClick={() => updateTodo(toDo)} disabled={toDo.status === "DONE"}>Advance</GreenButton>
         </div>
     )
 }
-
 export default ToDoCard;
 
+const RedButton = styled.button`
+  width: 100px;
+  height: 48px;
+  border-radius: 12px;
+  background-color: red;
+  color: black;
+  margin-top: 45px;
+`
+const YellowButton = styled.button`
+  width: 100px;
+  height: 48px;
+  border-radius: 12px;
+  background-color: yellow;
+  color: black;
+  margin-top: 45px;
+`
+const GreenButton = styled.button`
+  width: 100px;
+  height: 48px;
+  border-radius: 12px;
+  background-color: lightgreen;
+  color: black;
+  margin-top: 45px;
+`
